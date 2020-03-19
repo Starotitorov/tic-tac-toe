899841b831e521518532303bbed4859b0645f287
Artsiom Staratsitarau <starotitorov1997@gmail.com>
2020-03-20 01:43:30 +0300

    Update react-native version

--- components/AppNavigator.js
+++ components/AppNavigator.js
@@ -1,21 +1,19 @@
-import {
-    StackNavigator
-} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import WelcomeScreen from './WelcomeScreen';
 import GameScreen from './GameScreen';
 
-export default StackNavigator(
-    {
-        Welcome: {
-            screen: WelcomeScreen
-        },
-        Game: {
-            screen: GameScreen
-        }
+export default createStackNavigator(
+  {
+    Welcome: {
+      screen: WelcomeScreen,
     },
-    {
-        navigationOptions: {
-            title: 'Tic Tac Toe'
-        }
-    }
+    Game: {
+      screen: GameScreen,
+    },
+  },
+  {
+    navigationOptions: {
+      title: 'Tic Tac Toe',
+    },
+  },
 );
