b2f944ff67d71daae595427b09e06093912cf095
Artsiom Staratsitarau <starotitorov1997@gmail.com>
2020-03-19 23:48:10 +0300

    Add react-native-svg

--- components/GameScreen/Game/Circle.js
+++ components/GameScreen/Game/Circle.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import Icon from 'react-native-vector-icons/FontAwesome'
-import { FIGURE_SIZE } from './constants'
+import { Svg, Circle as C } from 'react-native-svg';
+import { FIGURE_SIZE, STROKE_WIDTH, FIGURE_PADDING } from './constants';
 
 const Circle = ({ color }) =>
     (
-        <Icon name="circle-o" color={color} size={FIGURE_SIZE} />
+        <Svg height={FIGURE_SIZE} width={FIGURE_SIZE}>
+            <C
+                cx={FIGURE_SIZE / 2}
+                cy={FIGURE_SIZE / 2}
+                r={(FIGURE_SIZE - 2 * FIGURE_PADDING) / 2}
+                stroke={color}
+                strokeWidth={STROKE_WIDTH}
+                fill="transparent"
+            />
+        </Svg>
     );
 
 export default Circle;
